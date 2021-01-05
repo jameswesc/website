8fd24a22137780f03a2becb008195f3e9647aec1
James Gregory <j.gregory@smashdelta.com>
2021-01-05 16:16:50 +1100

    added fonts

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,25 @@
 export default function Home() {
-    return <div>Hello World</div>
+    return (
+        <div>
+            <div className="p-4">
+                <a className="font-standard font-black text-xl" href="/">
+                    {' '}
+                    jwsc
+                </a>
+            </div>
+
+            <section>
+                <div className="container mx-auto">
+                    <h2 className="font-brand text-5xl">
+                        ✋ Hi
+                        <br />
+                        I'm James Wesc
+                        <br />
+                        I'm a Sydney based Designer / Developer currently
+                        working at Smash Delta.
+                    </h2>
+                </div>
+            </section>
+        </div>
+    )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
     purge: ['./pages/**/*.js', './components/**/*.js'],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                brand: ['NeueMachina', 'sans-serif'],
+                standard: ['Inter', 'sans-serif'],
+            },
+        },
     },
     variants: {
         extend: {},
