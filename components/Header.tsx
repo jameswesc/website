3805ee8e65fa98ecc755b3b660920772e3f9a7ea
James Gregory <j.gregory@smashdelta.com>
2021-01-07 18:23:29 +1100

    updated dropdown menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -23,6 +23,10 @@ export default function Header() {
                 </DropdownMenu.Trigger>
 
                 <DropdownMenu.Content className="py-2 grid gap-2">
+                    <DropdownMenu.Label className="font-standard text-xs text-pink-600">
+                        Active
+                    </DropdownMenu.Label>
+
                     <DropdownMenu.Item
                         as="a"
                         href="https://www.notion.so/jameswesc/Digital-Garden-8abe1bf736c14f26a002c3678d33dd7c"
@@ -30,6 +34,41 @@ export default function Header() {
                         className="p-2 bg-pink-100 hover:bg-pink-200 focus:bg-pink-200 outline-none font-brand underline"
                     >
                         🌺 Digital Garden
+                    </DropdownMenu.Item>
+
+                    {/* <DropdownMenu.Separator className="h-px bg-pink-500" /> */}
+
+                    <DropdownMenu.Label className="font-standard text-xs text-pink-400">
+                        Coming Soon
+                    </DropdownMenu.Label>
+
+                    <DropdownMenu.Item
+                        as="a"
+                        href="/"
+                        className="p-2 bg-pink-100  outline-none font-brand cursor-not-allowed underline "
+                    >
+                        <span className="opacity-50"> 🚧 About</span>
+                    </DropdownMenu.Item>
+                    <DropdownMenu.Item
+                        as="a"
+                        href="/"
+                        className="p-2 bg-pink-100  outline-none font-brand cursor-not-allowed underline "
+                    >
+                        <span className="opacity-50"> 🚧 Work</span>
+                    </DropdownMenu.Item>
+                    <DropdownMenu.Item
+                        as="a"
+                        href="/"
+                        className="p-2 bg-pink-100  outline-none font-brand cursor-not-allowed underline "
+                    >
+                        <span className="opacity-50"> 🚧 Experiments</span>
+                    </DropdownMenu.Item>
+                    <DropdownMenu.Item
+                        as="a"
+                        href="/"
+                        className="p-2 bg-pink-100  outline-none font-brand cursor-not-allowed underline "
+                    >
+                        <span className="opacity-50"> 🚧 Contact</span>
                     </DropdownMenu.Item>
                 </DropdownMenu.Content>
             </DropdownMenu.Root>
